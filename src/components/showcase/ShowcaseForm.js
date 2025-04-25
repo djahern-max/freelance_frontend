d842105c141677e84f0fc532304f724b67c2136b
djahern-max <danielaherniv@gmail.com>
2025-04-24 21:15:01 -0400

    update

--- src/components/showcase/ShowcaseForm.js
+++ src/components/showcase/ShowcaseForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useSelector } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { createShowcase } from '../../redux/showcaseSlice';
