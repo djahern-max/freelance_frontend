79bd4d1fac2e87e1a58d4f79fea4170b66c62079
djahern-max <danielaherniv@gmail.com>
2025-05-04 15:04:41 -0400

    update Home Page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -23,7 +23,7 @@ export default function HomePage() {
           <h1 className={styles.name}>Dane J Ahern</h1>
           <div className={styles.titleDivider}></div>
           <h2 className={styles.title}>Full-Stack Developer &</h2>
-          <h2 className={styles.title}>Workflow Automation Specialist</h2>
+          <h2 className={styles.title}>Certified Public Accountant</h2>
         </div>
 
         <p className={styles.subtitle}>What can I build for you?</p>
